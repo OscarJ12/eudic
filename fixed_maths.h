f4e73ae25b902e77231ed1e7a7e212829037f0fb
Oscar <35632447+OscarJ12@users.noreply.github.com>
2025-05-30 15:02:56 +0000

    both asm and C versions usable from .h file

--- fixed_maths.h
+++ fixed_maths.h
@@ -7,13 +7,26 @@
 extern "C" {
 #endif
 
+/**
+ * Multiply two Q16.16 fixed-point numbers
+ * @param a First operand
+ * @param b Second operand
+ * @return Result in Q16.16
+ */
 int32_t fixed_mul(int32_t a, int32_t b);
+
+/**
+ * Approximate exp(x) in Q16.16 format
+ * @param x Input in Q16.16
+ * @return Approximation of e^x in Q16.16
+ */
 int32_t fixed_exp(int32_t x);
 
+/** Constant for Q16.16 representation of 1.0 */
 #define Q16_ONE (1 << 16)
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif // FIXED_MATH_H
