b4d84e7ba512b1e7f7c83a5415d4f774a086c64b
Oscar <35632447+OscarJ12@users.noreply.github.com>
2025-06-18 15:14:34 +0100

    Update fixed_math.h
    Added logarithmic function

--- fixed_math.h
+++ fixed_math.h
@@ -30,8 +30,35 @@ int32_t fixed_exp(int32_t x);
  */
 int32_t fixed_exp_signed(int32_t x);
 
+/**
+ * Compute natural logarithm using binary search
+ * @param y Input in Q16.16 format (must be positive)
+ * @return ln(y) in Q16.16 format
+ */
+int32_t fixed_log(int32_t y);
+
+/**
+ * Compute base-10 logarithm
+ * @param y Input in Q16.16 format (must be positive)
+ * @return log10(y) in Q16.16 format
+ */
+int32_t fixed_log10(int32_t y);
+
+/**
+ * Compute base-2 logarithm
+ * @param y Input in Q16.16 format (must be positive)
+ * @return log2(y) in Q16.16 format
+ */
+int32_t fixed_log2(int32_t y);
+
 /** Q16.16 representation of 1.0 */
 #define Q16_ONE (1 << 16)
+
+/** Q16.16 representation of ln(2) */
+#define LN2_Q16 45426
+
+/** Q16.16 representation of ln(10) */
+#define LN10_Q16 150902
 
 #ifdef __cplusplus
 }
